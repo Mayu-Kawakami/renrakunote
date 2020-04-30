b3ad72cbf89471610b1de79b475f00f3d5b5dc1b
EC2 Default User <ec2-user@ip-172-31-29-135.us-east-2.compute.internal>
2020-04-30 09:16:46 +0000

    bladeファイル作成

---
+++ resources/views/notice.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>お知らせ</title>
+    </head>
+    <body>
+        <h1>お知らせ</h1>
+    </body>
+</html>
