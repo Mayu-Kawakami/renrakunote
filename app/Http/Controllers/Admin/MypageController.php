e611a47aad32b17101f00002d85eb04ef152466c
EC2 Default User <ec2-user@ip-172-31-29-135.us-east-2.compute.internal>
2020-05-15 08:34:42 +0000

    ルーティング、コントローラーの設定_5月15日

--- app/Http/Controllers/Admin/MypageController.php
+++ app/Http/Controllers/Admin/MypageController.php
@@ -7,9 +7,33 @@ use App\Http\Controllers\Controller;
 
 class MypageController extends Controller
 {
-    //
+    //保護者マイページ
+    public function index()
+    {
+        return view('admin.index');
+    }
+    
+    //プロフィール登録
     public function add()
     {
         return view('admin.create');
+    }
+    
+    //プロフィール編集
+    public function edit()
+    {
+        return view('admin.edit');
+    }
+    
+    //連絡ノート投稿
+    public function create()
+    {
+        return view('admin.renraku.create');
+    }
+    
+    //連絡ノート一覧
+    public function renraku()
+    {
+        return view('renraku.index');
     }
 }
--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class NoticeController extends Controller
 {
-    //
+    //お知らせ投稿
+    public function create()
+    {
+        return view('notice.create');
+    }
+    
+    //お知らせ編集
+    public function edit()
+    {
+        return view('notice.edit');
+    }
+    
+    //お知らせ一覧
+    public function index()
+    {
+        return view('notice.index');
+    }
+    
+    //(保育園から)連絡ノート投稿
+    public function renraku()
+    {
+        return view('renraku.create');
+    }
 }
