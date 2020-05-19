3ba24e18e9b9a60504cb8e31c048cbf9ed035981
EC2 Default User <ec2-user@ip-172-31-29-135.us-east-2.compute.internal>
2020-05-19 08:37:00 +0000

    トップページhtml_5月19日

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -7,9 +7,9 @@
 
         <title>トップページ</title>
     </head>
+    
     <body>
         <h1>○○保育園連絡帳</h1>
-        
       <section>    
         <h2 class="information">お知らせ</h2>
         <ul class="information-list">
@@ -27,10 +27,34 @@
           <p>会員情報の登録・編集</p>
           <p>お子さまのプロフィール登録・編集</p>
           <p><a href="{{ action('Admin\MypageController@add') }}">設定</a></p>
-          <p><img src="/renrakunote/image/key.png" width="327" height="214"></p>
+          <p><img src="./renrakunote/public/images/key.png" width="327" height="214"></p>
+        </div>
+        
+        <div class="renraku-note">
+          <h2>連絡ノート</h2>
+          <p>お子さまの健康状態、投薬依頼など</p>
+          <p><a href="{{ action('Admin\MypageController@create') }}">連絡ノートを書く</a></p>
+          <p><img src="./renrakunote/public/images/renrakunote.png" width="327" height="214"></p>
+        </div>
+        
+        <div class="blog">
+          <h2>今日のできごと</h2>
+          <p>園での様子やお知らせ</p>
+          <p><a href="{{ action('Admin\MypageController@renraku') }}">詳しく見る</a></p>
+          <p><img src="./renrakunote/public/images/picture.png" width="327" height="214"></p>
         </div>
       </section>
       
-      
+      <footer>
+        <div class="footer">
+          <p>○○保育園</p>
+          <ul>
+            <li>〒123-4567 〇〇県〇〇市〇〇町1-2-3</li>
+            <li>TEL:0000-123-4567</li>
+            <li>FAX:0000-123-4568</li>
+          </ul>
+        </div>
+      </footer>
     </body>
+    
 </html>
