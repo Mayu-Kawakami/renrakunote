58804868dbbaaeeedb80bf4d76fd87a96ea0caef
EC2 Default User <ec2-user@ip-172-31-29-135.us-east-2.compute.internal>
2020-05-21 09:03:51 +0000

    お知らせ一覧html_5月21日

--- resources/views/notice/index.blade.php
+++ resources/views/notice/index.blade.php
@@ -8,6 +8,54 @@
         <title>（保育園から）お知らせ一覧</title>
     </head>
     <body>
-        <h1>（保育園から）お知らせ一覧</h1>
+        <header>
+            <div class="header-title-area">
+                <h1>お知らせ</h1>
+            </div>
+        </header>
+        
+        <div class="notice-index">
+            <div class="notice-left-contents">
+                <div class="notice-date">2020年月○日</div>
+                <h2 class="notice-title">お知らせタイトル</h2>
+            </div>
+            <div class="notice-right-contents">
+                <p class="notice-text">お知らせ本文が入ります。お知らせ本文が入ります。お知らせ本文が入ります。</p>
+            </div>
+        </div>
+        <div class="notice-index">
+            <div class="notice-left-contents">
+                <div class="notice-date">2020年月○日</div>
+                <h2 class="notice-title">お知らせタイトル</h2>
+            </div>
+            <div class="notice-right-contents">
+                <p class="notice-text">お知らせ本文が入ります。お知らせ本文が入ります。お知らせ本文が入ります。</p>
+            </div>
+        </div>
+        <div class="notice-index">
+            <div class="notice-left-contents">
+                <div class="notice-date">2020年月○日</div>
+                <h2 class="notice-title">お知らせタイトル</h2>
+            </div>
+            <div class="notice-right-contents">
+                <p class="notice-text">お知らせ本文が入ります。お知らせ本文が入ります。お知らせ本文が入ります。</p>
+            </div>
+        </div>
+        
+        <div class="button">
+            <p><p><a href="{{ action('ToppageController@index') }}">TOPへ戻る</a></p></p>
+        </div>
+        
+      <footer>
+        <div class="footer">
+          <p>○○保育園</p>
+          <ul>
+            <li>〒123-4567 〇〇県〇〇市〇〇町1-2-3</li>
+            <li>TEL:0000-123-4567</li>
+            <li>FAX:0000-123-4568</li>
+          </ul>
+        </div>
+      </footer>
+
     </body>
 </html>
