33b1aee2d24edac6e2bbf20ff3719c86e2b6cf62
EC2 Default User <ec2-user@ip-172-31-29-135.us-east-2.compute.internal>
2020-06-05 08:00:46 +0000

    6月5日

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/', 'Admin\MypageController@index'); //保護者マイページ
-Route::get('admin/create', 'Admin\MypageController@add');  //プロフィール登録
-Route::get('admin/edit', 'Admin\MypageController@edit');  //プロフィール
-Route::get('admin/renraku/create', 'Admin\MypageController@create'); //保護者からの連絡ノート投稿
-Route::get('renraku/', 'Admin\MypageController@renraku'); //連絡ノート一覧
+Route::get('admin/', 'Admin\MypageController@index')->middleware('auth'); //保護者マイページ
+Route::get('admin/create', 'Admin\MypageController@add')->middleware('auth');  //プロフィール登録
+Route::get('admin/edit', 'Admin\MypageController@edit')->middleware('auth');  //プロフィール
+Route::get('admin/renraku/create', 'Admin\MypageController@create')->middleware('auth'); //保護者からの連絡ノート投稿
+Route::get('renraku/', 'Admin\MypageController@renraku')->middleware('auth'); //連絡ノート一覧
 
 
 Route::get('notice/create', 'NoticeController@create'); //お知らせ投稿
@@ -30,6 +30,14 @@ Route::post('notice/create', 'NoticeController@create'); //お知らせ投稿
 Route::get('notice/', 'NoticeController@index'); //お知らせ一覧
 Route::get('renraku/create', 'NoticeController@renraku'); //(保育園から)連絡ノート投稿
 
-Route::get('/', 'ToppageController@index'); //トップページ
+Route::get('/', 'ToppageController@index')->middleware('auth'); //トップページ
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
