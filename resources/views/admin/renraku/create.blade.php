0a563972f8d2e8b500fc3d4b5b9f66b01c58bce6
EC2 Default User <ec2-user@ip-172-31-29-135.us-east-2.compute.internal>
2020-06-16 07:40:51 +0000

    6/16_インデントを揃える

--- resources/views/admin/renraku/create.blade.php
+++ resources/views/admin/renraku/create.blade.php
@@ -21,35 +21,75 @@
                         <input type="number"  class="form-control" name="taion">℃
                     </div>
                 </div>
+                <div class="form-group row">
+                    <label class="col-md-2 text-right">朝食：</label>
+                    <div class="col-md-8">
+                        <input type="text" class="form-control" name="breakfast">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2 text-right">便：</label>
+                        <div class="row col-md-8">
+                        <label><input type="radio" name="ben" value="あり">あり</label>
+                        <label><input type="radio" name="ben" value="なし">なし</label>
+                    </div>
             
-            <div class="form-group row">
-                <label class="col-md-2 text-right">朝食：</label>
-                <div class="col-md-8">
-                    <input type="text" class="form-control" name="breakfast">
                 </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-md-2 text-right">便：</label>
+                <div class="form-group row">
+                    <label class="col-md-2 text-right">薬：</label>
                     <div class="row col-md-8">
-                    <label><input type="radio" name="ben" value="あり">あり</label>
-                    <label><input type="radio" name="ben" value="なし">なし</label>
+                        <label><input type="radio" name="medicine" value="あり">あり</label>
+                        <label><input type="radio" name="medicine" value="なし">なし</label>
+                        <p>※「あり」の場合は投薬依頼を登録して下さい。</p>
+                    </div>
                 </div>
-        
-            </div>
-            <div class="form-group row">
-                <label class="col-md-2 text-right">薬：</label>
-                <div class="row col-md-8">
-                    <label><input type="radio" name="medicine" value="あり">あり</label>
-                    <label><input type="radio" name="medicine" value="なし">なし</label>
-                    <p>※「あり」の場合は投薬依頼を登録して下さい。</p>
+                <div class="form-group row">
+                    <label class="col-md-2 text-right">連絡事項：</label>
+                    <div class="col-md-8">
+                    <textarea class="form-control" name="renraku" rows="10"></textarea>
+                    </div>
+                </div>
+                
+                    {{ csrf_field() }}
+                
+                <div class="row text-center">
+                    <div class="col-12">
+                    <input type="submit" class="btn btn-primary mb-3" value="登録">
+                    </div>
                 </div>
             </div>
-            <div class="form-group row">
-                <label class="col-md-2 text-right">連絡事項：</label>
-                <div class="col-md-8">
-                <textarea class="form-control" name="renraku" rows="10"></textarea>
-                </div>
+            <div class="row">
+              <h2>投薬依頼</h2>
             </div>
+            <div class="border p-4 mb-5">
+                <div class="form-group row">
+                    <label class="col-md-2 text-right">病院名：</label>
+                    <div class="col-md-8">
+                        <input type="text" class="form-control" name="hospital">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2 text-right">症状：</label>
+                    <div class="col-md-8">
+                        <input type="text" class="form-control" name="symptom">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2 text-right">与薬時間：</label>
+                    <div class="row col-md-8">
+                        <label><input type="radio" name="time" value="食前">食前</label>
+                        <label><input type="radio" name="time" value="食後">食後</label>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2 text-right">薬の種類：</label>
+                    <div class="row col-md-8 text-inline">
+                        <label class="col-md-2 text-right"><input type="radio" name="type" value="粉">粉</label>
+                        <label class="col-md-3 text-right"><input type="radio" name="type" value="シロップ">シロップ</label>
+                        <label class="col-md-3 text-right"><input type="radio" name="type" value="錠剤">錠剤</label>
+                        <label class="col-md-3 text-right"><input type="radio" name="type" value="外用薬">外用薬</label>
+                    </div>
+                </div>
             
                 {{ csrf_field() }}
             
@@ -59,51 +99,8 @@
                     </div>
                 </div>
             </div>
-    
-
-    <div class="row">
-        <h2>投薬依頼</h2>
+        </form>
     </div>
-    <div class="border p-4 mb-5">
-        <div class="form-group row">
-            <label class="col-md-2 text-right">病院名：</label>
-            <div class="col-md-8">
-                <input type="text" class="form-control" name="hospital">
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-md-2 text-right">症状：</label>
-            <div class="col-md-8">
-                <input type="text" class="form-control" name="symptom">
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-md-2 text-right">与薬時間：</label>
-            <div class="row col-md-8">
-                <label><input type="radio" name="time" value="食前">食前</label>
-                <label><input type="radio" name="time" value="食後">食後</label>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-md-2 text-right">薬の種類：</label>
-            <div class="row col-md-8 text-inline">
-                <label class="col-md-2 text-right"><input type="radio" name="type" value="粉">粉</label>
-                <label class="col-md-3 text-right"><input type="radio" name="type" value="シロップ">シロップ</label>
-                <label class="col-md-3 text-right"><input type="radio" name="type" value="錠剤">錠剤</label>
-                <label class="col-md-3 text-right"><input type="radio" name="type" value="外用薬">外用薬</label>
-            </div>
-        </div>
-    
-        {{ csrf_field() }}
-    
-        <div class="row text-center">
-            <div class="col-12">
-            <input type="submit" class="btn btn-primary mb-3" value="登録">
-            </div>
-        </div>
-    </div>
-    </form>
-
 <!--container-->
 </div>        
 @endsection
