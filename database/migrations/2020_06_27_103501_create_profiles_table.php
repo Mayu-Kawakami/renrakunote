fe48945b85b986b3cc4bd07ce08ae169867a62f0
EC2 Default User <ec2-user@ip-172-31-29-135.us-east-2.compute.internal>
2020-06-27 11:27:21 +0000

    profilesテーブル途中まで_2

--- database/migrations/2020_06_27_103501_create_profiles_table.php
+++ database/migrations/2020_06_27_103501_create_profiles_table.php
@@ -15,6 +15,8 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('nickname');
+            $table->string('gender');
             $table->timestamps();
         });
     }
